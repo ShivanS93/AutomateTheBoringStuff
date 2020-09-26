c78dea0e32527023da1c2ba86e7077efdb3ff4b9
Shivan <shivan@shivansivakumaran.com>
2020-09-26 12:02:59 +1200

    first commit

---
+++ Chapter3/theCollatzSequence.py
@@ -0,0 +1,15 @@
+#! python3
+
+# theCollatzSequence.py - This uses the Collatz Sequence on an input.
+
+# Rules:
+# if num is even, then return num // 2
+# if num is off, then return 3 * num + 1
+# this will always lead to one
+
+def collatz(num):
+    '''
+    num - number to be added to the collatz sequence
+    '''
+
+    pass
